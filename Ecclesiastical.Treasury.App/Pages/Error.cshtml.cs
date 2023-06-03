febf31688b59b6e203f453dffca9743234fefbe1
ivanmelingibb <ivan.meling.ibb@gmail.com>
2023-06-02 19:35:05 -0700

    Changes in template

--- Ecclesiastical.Treasury.App/Pages/Error.cshtml.cs
+++ Ecclesiastical.Treasury.App/Pages/Error.cshtml.cs
@@ -5,18 +5,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Ecclesiastical.Treasury.App.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
     private readonly ILogger<ErrorModel> _logger;
 
     public ErrorModel(ILogger<ErrorModel> logger)
     {
         _logger = logger;
     }
-
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
     public void OnGet()
     {
